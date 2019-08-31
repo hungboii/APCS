cfc2dacce46b6a66cca97c0f719be24f8ce173f6
hungboii <mafewh119@gmail.com>
2019-08-31 12:31:55 -0700

    yes

---
+++ EvenOrOdd/OddorEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class OddorEven
+{
+    public static void main(String[] args)
+    {
+        Scanner leadership = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int corrupt = leadership.nextInt();
+        if (corrupt % 2 == 0)
+        {
+            System.out.println("The integer " + corrupt + " is even." );
+        }
+        else 
+        {
+            System.out.println("The integer " + corrupt + " is odd.");
+        }
+
+    }
+}
