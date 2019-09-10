fd2c66a542bfe7bba781824448ed8a9b11a29208
hungboii <mafewh119@gmail.com>
2019-09-10 16:58:02 -0700

    oop

---
+++ NameReversal/reversalname.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class reversalname
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Please print your name. ");
+        String name = keyboard.nextLine();
+        for(int len = name.length(); len > 0; len--)
+        {
+            String reverse = name.substring(len - 1, len);
+            System.out.print(reverse.toLowerCase());
+        }
+    }
+}
