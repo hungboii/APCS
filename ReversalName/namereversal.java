aa6042416841f092b18086d73686b27c1dcac6e3
Matthew Hung <mafewh119@gmail.com>
2019-09-10 16:29:59 -0700

    Create namereversal.java

---
+++ ReversalName/namereversal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class reversalname
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print = "Please print your name. ";
+        String name = keyboard.nextLine();
+        for()
+
+        String reverse = 
+        System.out.print(reverse.toLowerCase());
+    }
+}
