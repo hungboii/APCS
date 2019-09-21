d6e9e7aaef0eebb1776f3462693783da4b00d2d5
hungboii <mafewh119@gmail.com>
2019-09-21 10:19:36 -0700

    Update planetweight.java

--- PlanetWeight/planetweight.java
+++ PlanetWeight/planetweight.java
@@ -5,43 +5,51 @@ public class planetweight
     {
         Scanner kb = new Scanner(System.in);
         System.out.print("What is your weight on the Earth? ");
-        int weight = kb.nextInt();
+        double weight = kb.nextDouble();
         System.out.println();
 
-        System.out.println("1. Mercury");
-        System.out.println("2. Venus");
-        System.out.println("3. Mars");
-        System.out.println("4. Jupiter");
-        System.out.println("5. Saturn");
-        System.out.println("6. Uranus");
-        System.out.println("7. Neptune");
+        System.out.println("M. Mercury");
+        System.out.println("V. Venus");
+        System.out.println("R. Mars");
+        System.out.println("J. Jupiter");
+        System.out.println("S. Saturn");
+        System.out.println("U. Uranus");
+        System.out.println("N. Neptune");
         System.out.println();
 
         System.out.print("Selection? ");
-        int choice = kb.nextInt();
+        String choice = kb.next();
+        char chr = choice.charAt(0);
         System.out.println(" ");
 
-        switch(choice)
+        switch(chr)
         {
-            case 1:
+            case 'm':
+            case 'M':
                 System.out.print("Your weight on Mercury would be " + (weight / 9.8 * 3.7));
                 break;
-            case 2:
+            case 'v':
+            case 'V':
                 System.out.print("Your weight on Venus would be " + (weight / 9.8 * 8.87));
                 break;
-            case 3:
+            case 'r':
+            case 'R':
                 System.out.print("Your weight on Mars would be " + (weight / 9.8 * 3.711));
                 break;
-            case 4:
+            case 'j':
+            case 'J':
                 System.out.print("Your weight on Jupiter would be " + (weight / 9.8 * 24.79));
                 break;
-            case 5:
+            case 's':
+            case 'S':
                 System.out.print("Your weight on Saturn would be " + (weight / 9.8 * 10.44));
                 break;
-            case 6:
+            case 'u':
+            case 'U':
                 System.out.print("Your weight on Uranus would be " + (weight / 9.8 * 8.87));
                 break;
-            case 7:
+            case 'n':
+            case 'N':
                 System.out.print("Your weight on Neptune would be " + (weight / 9.8 * 11.15));
         }
     }
