80ba19fe20c64a0b5c12f351c1c9b22510ea993f
hungboii <mafewh119@gmail.com>
2019-09-25 20:41:51 -0700

    Initial commit

---
+++ quadraticfun.java
@@ -0,0 +1,16 @@
+public class quadraticfun
+{
+    public static void main(String[] args)
+    {
+        double x = double.parsedouble(args[0]);
+        double y = double.parsedouble(args[1]);
+        double z = double.parsedouble(args[2]);
+
+        System.out.print(equation(args[1]);
+    }
+    public static double equation(double x, y ,z)
+    {
+        double answer = (Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / 2 * a;
+        return answer;
+    }
+}
