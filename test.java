caf9737c850f90b77773439e5ede76ed1012c4bd
hungboii <mafewh119@gmail.com>
2019-10-01 18:08:01 -0700

    Update test.java

--- test.java
+++ test.java
@@ -2,20 +2,8 @@ public class test
 {
     public static void main(String[] args)
     {
-        Double a = Double.parseDouble(args[0]);
-        Double b = Double.parseDouble(args[1]);
-        Double c = Double.parseDouble(args[2]);
-
-        System.out.println("The positive x value is " + littleMethod(a, b, c));
-    }
-
-    public static Double littleMethod(Double a, Double b, Double c)
-    {
-        Double radical = Math.sqrt(Math.pow(b, 2) - (4 * a * c));
-        Double twoA = 2 * a;
-        
-        Double answer = (0 - b + radical) / twoA;
-        return answer;
+       int i = 52;
+       if((i / 10 + i % 10) % 7 == 0)
+        System.out.print("yup");
     }
 }
-    
