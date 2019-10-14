241b0117aa9e5fed98f82c59a0a61f668379d70f
Matthew Hung <mafewh119@gmail.com>
2019-10-14 16:37:17 -0700

    Initial commit

--- Diameter15/Circle.java
+++ Diameter15/Circle.java
@@ -4,16 +4,6 @@ public class Circle
     {
         radius = r;
     }
-    public double area()
-    {
-        double a = Math.PI * radius * radius;
-        return a;
-    }
-    public double circumference()
-    {
-        double c = 2 * Math.PI * radius;
-        return c;
-    }
     public double diameter()
     {
         double d = 2 * radius;
