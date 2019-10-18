70701d69d558e88b329a78cc665cafc64ab95a89
hungboii <mafewh119@gmail.com>
2019-10-17 20:43:53 -0700

    Initial commit

---
+++ HelloWorld1/HelloWorldApp.java
@@ -0,0 +1,14 @@
+public class HelloWorldApp 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("      te    st     ");
+		System.out.println("      te    st     ");
+		System.out.println("      te    st     ");
+		System.out.println("      te    st     ");
+		System.out.println("test            test");
+		System.out.println("  test        test  ");
+		System.out.println("    test    test    ");
+		System.out.println("        test        ");	   
+	}
+}
