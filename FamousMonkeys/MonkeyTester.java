99a37bc830aac31bd27b857d1e483a8e9df7828c
hungboii <mafewh119@gmail.com>
2019-12-01 19:31:07 -0800

    Initial commit

--- FamousMonkeys/MonkeyTester.java
+++ FamousMonkeys/MonkeyTester.java
@@ -6,7 +6,7 @@ public class MonkeyTester
         bots[0] = new Nick("Nick", "Chan", "cos0cospi/2cos0");
         bots[1] = new Zoe("Zoe", "Moore", "reeee.");
         bots[2] = new Luke("Luke", "Wonzen", "bruh.");
-        bots[3] = new Andrew("Andrew", "Cho", "I am monkey.");
+        bots[3] = new Andrew("Andrew", "Cho", "oo oo ah ah");
 
         for(FamousMonkeys  monkey : bots)
             monkey.speak();
