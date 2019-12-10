4f47f97bd47a21018d25662aa03e8176552ab380
Matthew Hung <mafewh119@gmail.com>
2019-12-09 21:53:36 -0800

    Update ChristmasTree.java

--- Graphics/ChristmasTree.java
+++ Graphics/ChristmasTree.java
@@ -10,8 +10,8 @@ public class ChristmasTree
         Graphics g;
       
         win = new JFrame("bruh");
-        win.setSize(400, 500);
-        win.setLocation(800, 40);
+        win.setSize(500, 600);
+        win.setLocation(400, 50);
         win.setVisible(true);
       
         contentPane = win.getContentPane();
@@ -20,7 +20,7 @@ public class ChristmasTree
         try {Thread.sleep(400);} catch (Exception e) {}
           
         g.setColor(new Color(135, 206, 250)); //background
-        g.fillRect(0, 0, 400, 500);
+        g.fillRect(0, 0, 500, 600);
 
         drawTree(win, contentPane, g);
 
@@ -34,27 +34,27 @@ public class ChristmasTree
             if(i % 90 >= 60)
                 g.setColor(Color.BLUE);
 
-            g.fillOval(195, 140, 10, 10);
+            g.fillOval(245, 190, 10, 10);
 
-            g.fillOval(195, 205, 10, 10);
-            g.fillOval(245, 205, 10, 10);
-            g.fillOval(145, 205, 10, 10);
+            g.fillOval(245, 255, 10, 10);
+            g.fillOval(295, 255, 10, 10);
+            g.fillOval(195, 255, 10, 10);
 
-            g.fillOval(195, 280, 10, 10);
-            g.fillOval(130, 280, 10, 10);
-            g.fillOval(260, 280, 10, 10);
+            g.fillOval(245, 350, 10, 10);
+            g.fillOval(175, 350, 10, 10);
+            g.fillOval(315, 350, 10, 10);
 
-            g.fillOval(195, 355, 10, 10);
-            g.fillOval(115, 355, 10, 10);
-            g.fillOval(275, 355, 10, 10);
+            g.fillOval(245, 450, 10, 10);
+            g.fillOval(150, 450, 10, 10);
+            g.fillOval(340, 450, 10, 10);
 
             g.setColor(Color.WHITE); //snow
-            g.fillOval((int)(Math.random() * 400), (int)(Math.random() * 500), 4, 4);
+            g.fillOval((int)(Math.random() * 500), (int)(Math.random() * 600), 4, 4);
 
             if(i % 500 == 0) 
             {
                 g.setColor(new Color(135, 206, 250)); 
-                g.fillRect(0, 0, 400, 500); //get rid of the snow by wiping the screen clean 
+                g.fillRect(0, 0, 500, 600); //get rid of the snow by wiping the screen clean 
                 drawTree(win, contentPane, g); //redraw tree after wiping screen
             }
             try {Thread.sleep(10);} catch (Exception e) {}
@@ -66,37 +66,37 @@ public class ChristmasTree
         g.setColor(new Color(34, 139, 34));
                         
         Polygon topTree = new Polygon();
-        topTree.addPoint(200, 100);
-        topTree.addPoint(125, 225);
-        topTree.addPoint(275, 225);
+        topTree.addPoint(250, 150);
+        topTree.addPoint(175, 275);
+        topTree.addPoint(325, 275);
         g.fillPolygon(topTree);
 
         Polygon middle = new Polygon();
-        middle.addPoint(145, 225);
-        middle.addPoint(255, 225);
-        middle.addPoint(300, 300);
-        middle.addPoint(100, 300);
+        middle.addPoint(195, 275);
+        middle.addPoint(305, 275);
+        middle.addPoint(350, 375);
+        middle.addPoint(150, 375);
         g.fillPolygon(middle);
 
         Polygon bottom = new Polygon();
-        bottom.addPoint(120, 300);
-        bottom.addPoint(280, 300);
-        bottom.addPoint(325, 375);
-        bottom.addPoint(75, 375);
+        bottom.addPoint(170, 375);
+        bottom.addPoint(330, 375);
+        bottom.addPoint(375, 475);
+        bottom.addPoint(125, 475);
         g.fillPolygon(bottom);
 
         g.setColor(new Color(150, 75, 0));
 
-        g.fillRect(175, 375, 50, 75); //base/stump of tree  
+        g.fillRect(225, 475, 50, 75); //base/stump of tree  
 
         g.setColor(Color.WHITE);
 
-        g.fillRect(0, 450, 400, 25); //snow on ground
+        g.fillRect(0, 550, 500, 25); //snow on ground
         
         g.setColor(Color.YELLOW);
 
-        int[] x = {200, 215, 210, 225, 205, 200, 195, 175, 190, 185};
-        int[] y = {95, 105, 90, 80, 80, 64, 80, 80, 90, 105};
+        int[] x = {250, 265, 260, 275, 255, 250, 245, 225, 240, 235};
+        int[] y = {145, 155, 140, 130, 130, 114, 130, 130, 140, 155};
         g.fillPolygon(x, y, 10); //star on top
    }
 }
