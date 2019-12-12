41fc68d87b9b71ac7b49b6592b564615b4c9fd77
Matthew Hung <mafewh119@gmail.com>
2019-12-12 08:58:07 -0800

    Initial commit

--- test.java
+++ test.java
@@ -2,23 +2,6 @@ public class test
 {
     public static void main(String[] args)
     {
-        for(int i = 1; i > 0; i++)
-        {
-            if(i % 3 == 1)
-                System.out.print("yes");
-            if(i % 3 == 2)
-                System.out.print("no");
-            if(i % 3 == 0)
-                System.out.print("nbot");
-
-            // if(i % 3 == 0)
-            //     g.setColor(new Color(135,206,250));
-            //     g.fillOval(200, 200, 10, 10);
-            //     g.fillOval(200, 250, 10, 10);
-            //     g.setColor(Color.WHITE);
-            //     g.fillOval(200, 300, 10, 10);
-
-            try {Thread.sleep(400);} catch (Exception e) {}
-        }
+        System.out.print("bruh");
     }
 }
