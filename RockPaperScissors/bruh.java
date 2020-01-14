c98c859be775bc61b6bf147a95ae7f419299a850
Matthew Hung <mafewh119@gmail.com>
2020-01-13 18:38:08 -0800

    Initial commit

---
+++ RockPaperScissors/bruh.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class bruh
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        while(true)
+        {
+            System.out.print("Rock-Paper-Scissors - Pick your weapon [R, P, S]: ");
+            String playerChoice = kb.next();
+
+            if(!(playerChoice.equals("R")||playerChoice.equals("P")||playerChoice.equals("S")))
+            {
+                System.out.println("please do not break my program.");
+                continue;
+            }
+
+            RockPaperScissors newGame  = new RockPaperScissors(playerChoice);
+            System.out.println(newGame);
+            System.out.println(newGame.determineWinner());
+            System.out.println("");
+            System.out.print("Do you want to play again? ");
+
+            if(kb.next().equals("n"))
+                break;
+            System.out.println("");
+        }
+
+    }
+}
