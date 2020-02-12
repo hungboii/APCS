b42b3dab780d8b97ead65cdec8efe0cba02f6991
Matthew Hung <mafewh119@gmail.com>
2020-02-12 13:38:27 -0800

    joe

--- GridWorldCode/projects/BattleBug/CoronavirusRockRunner.java
+++ GridWorldCode/projects/BattleBug/CoronavirusRockRunner.java
@@ -15,7 +15,7 @@ public class CoronavirusRockRunner
         world.add(new Location(3, 3), new Bug());
         world.add(new Location(2, 8), new Bug());
         world.add(new Location(5, 5), new Rock(Color.PINK));
-        world.add(new Location(1, 5), new Bug());
+        world.add(new Location(1, 5), new Critter());
         world.add(new Location(7, 2), new Bug(Color.YELLOW));
         world.add(new Location(4, 4), new CoronavirusRock());
         world.show();
--- GridWorldCode/projects/BattleBug/VirusCritter.java
+++ GridWorldCode/projects/BattleBug/VirusCritter.java
@@ -1,30 +1,28 @@
-import java.awt.Color;
+package info.gridworld.actor;
+
+import info.gridworld.grid.Location;
+
 import java.util.ArrayList;
 
-import javax.tools.DocumentationTool.Location;
-
-import info.gridworld.actor.*;
-import info.gridworld.grid.*;
-import info.gridworld.world.*;
-
-public class CoronavirusRock extends Rock 
+public class VirusCritter extends Critter 
 {
     private ArrayList<Integer> turnCounter = new ArrayList<Integer>();
     private ArrayList<Actor> infected = new ArrayList<Actor>();
 
-    public CoronavirusRock()
+    public VirusCritter()
     {
         setColor(Color.GREEN);
         for (int i = 0; i < 100; i++) 
             turnCounter.add(3);  
     }
-    public void getActorsFirst()
+    public ArrayList<Actor> getActorsFirst()
     {
-        infected = getGrid().getNeighbors(getLocation()); //rocks don't die to virus? only carry it? or make a critter that moves
+        return getGrid().getNeighbors(getLocation()); //rocks don't die to virus? only carry it? or make a critter that moves
     }
-    public void getActors()
+    public ArrayList<Actor> getActors()
     {
-        int origSize = infected.size();
+        ArrayList<Actor> virus = infected;
+        int origSize = virus.size();
 
         for(int i = 0; i < origSize; i++)
         {
@@ -34,18 +32,20 @@ public class CoronavirusRock extends Rock
                 // info.gridworld.grid.Location virus = infected.get(i).getLocation();
                 // System.out.println(virus);
                 // newInfected = getGrid().getNeighbors(virus);
-                newInfected = getGrid().getNeighbors(infected.get(i).getLocation());
+                newInfected = getGrid().getNeighbors(virus.get(i).getLocation());
             } catch (NullPointerException e) {}
 
             for(Actor hugh : newInfected)
             {
-                if(!(hugh instanceof CoronavirusRock || infected.contains(hugh)))
-                    infected.add(hugh);
+                if(!(hugh instanceof CoronavirusRock || virus.contains(hugh)))
+                    virus.add(hugh);
             }
         }
+        return virus;
+        
     }
 
-    public void infectActors()
+    public void processActors(ArrayList<Actor> infected)
     {
         for(int i = 0; i < infected.size(); i++)
         {
@@ -68,15 +68,18 @@ public class CoronavirusRock extends Rock
     public void act()
     {
         if(infected == null)
-            getActorsFirst();
+            infected = getActorsFirst();
         else if(infected.isEmpty())
         {
-            getActorsFirst();
+            infected = getActorsFirst();
         }
         else
         {
             getActors();
-            infectActors();
+            processActors(infected);
+            ArrayList<Location> moveLocs = getMoveLocations();
+            Location loc = selectMoveLocation(moveLocs);
+            makeMove(loc);
         }
     }   
 
