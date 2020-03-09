8c6502b3cf3e0c5fb75cac1e73a112a873dbbe63
hungboii <mafewh119@gmail.com>
2020-03-09 16:58:32 -0700

    Update MergeSort.java

--- Blue Pelican Projects/Project 41/MergeSort.java
+++ Blue Pelican Projects/Project 41/MergeSort.java
@@ -3,7 +3,7 @@ public class MergeSort
     public static void main(String[] args)
     {
         int[] theArray = {4, 2, 5, 1, 3, 18, 0, 9, 6};
-        sort(theArray);
+        sort(theArray, 0, theArray.length - 1);
 
         for(int j = 0; j < theArray.length; j++)
             System.out.print(theArray[j] + "  ");
@@ -22,6 +22,39 @@ public class MergeSort
 
     public static void merge(int[] a, int left, int middle, int right)
     {
+        int[] tmpArray = new int[right - left + 1];
 
+        int index1 = left;
+        int index2 = middle + 1;
+        int index = 0;
+
+        while(index1 <= middle && index2 <= right)
+        {
+            if(a[index1] < a[index2])
+            {
+                tmpArray[index] = a[index1];
+                index1++;
+            }
+            else
+            {
+                tmpArray[index] = a[index2];
+                index2++;
+            }
+            index++;
+        }
+        while(index1 <= middle)
+        {
+            tmpArray[index] = a[index1];
+            index1++;
+            index++;
+        }
+        while(index2 <= right)
+        {
+            tmpArray[index]= a[index2];
+            index2++;
+            index++;
+        }
+        for(index = 0; index <  tmpArray.length; index++)
+            a[left + index]  = tmpArray[index];
     }
 }
